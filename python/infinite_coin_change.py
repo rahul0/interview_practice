dba08a68c927ac50892560ba0cf64fcba867fdce
Rahul Kumar <rahul.rvce@gmail.com>
2015-05-16 00:01:44 -0700

    infinite coin change problems

---
+++ python/infinite_coin_change.py
@@ -0,0 +1,17 @@
+
+
+def make_coin_change ( coins, total,  partial_sum,  index, coin_used):
+    #mean we have processed all coins
+    if index> len(coins):
+        return
+
+    if partial_sum
+    if partial_sum==total:
+        partial_sum=partial_sum+1
+        make_coin_change(coins, )
+
+
+if __name__=='__main__':
+    pass
+else:
+    pass
